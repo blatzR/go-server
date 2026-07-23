dc1a9beb30a83f920be9c8ac28b9034abbdcd814
blatzR <rahulblatz002@gmail.com>
2026-07-23 08:09:53 +0530

    Updated server code

--- server.go
+++ server.go
@@ -6,9 +6,21 @@ import (
 	"net/http"
 )
 
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, World!")
+}
+
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintln(w, "ok")
+}
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Hello World!")
-	})
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	http.HandleFunc("/", helloHandler)
+	http.HandleFunc("/healthz", healthHandler)
+
+	log.Println("Server starting on port 8080...")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
 }
